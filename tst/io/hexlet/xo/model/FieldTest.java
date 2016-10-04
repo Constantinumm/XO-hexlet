2430fc9fea915db5a86f279fe639fd29fff67928
Constantinum <constantinumm@gmail.com>
2016-10-04 05:28:51 +0300

    FieldTests with new Field (with flexible size)

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -1,6 +1,5 @@
 package io.hexlet.xo.model;
 
-import io.hexlet.xo.model.exceptions.AlreadyOccupiedExceptions;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 import org.junit.Test;
 
@@ -13,14 +12,14 @@ public class FieldTest {
 
     @Test
     public void testGetSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(3, field.getSize());
     }
 
     @Test
     public void testSetFigure() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
 
@@ -33,7 +32,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureNotSet() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -44,7 +43,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsLessThenZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, -1);
 
         try {
@@ -57,7 +56,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThenZero() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1, 0);
 
         try {
@@ -70,7 +69,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(field.getSize() + 1, 0);
 
         try {
@@ -83,7 +82,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYMoreThenSize() throws Exception {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, field.getSize() + 1);
 
         try {
