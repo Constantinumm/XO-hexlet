bb88f2e50a281819132a517ccfd53d3a790e9c94
Constantinum <constantinumm@gmail.com>
2016-10-04 03:02:07 +0300

    Field - tests for the point added

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,11 +1,18 @@
 package io.hexlet.xo.model;
 
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedExceptions;
+import io.hexlet.xo.model.exceptions.InvalidPointExceptions;
+
 import java.awt.*;
 
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+
+    private static final int MIN_COORDINATE = 0;
+
+    private static final int MAX_COORDINATE = FIELD_SIZE;
 
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
@@ -13,11 +20,28 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) {
+    public Figure getFigure(final Point point) throws InvalidPointExceptions {
+        if (!checkPoint(point)) {
+            throw new InvalidPointExceptions();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure) {
+    public void setFigure(final Point point, final Figure figure)  throws InvalidPointExceptions, AlreadyOccupiedExceptions {
+        if (!checkPoint(point)) {
+            throw new InvalidPointExceptions();
+        }
+        if (field[point.x][point.y] != null) {
+            throw new AlreadyOccupiedExceptions();
+        }
         field[point.x][point.y] = figure;
+    }
+
+    private boolean checkPoint(final Point point) {
+        return checkCordinate(point.x) && checkCordinate(point.y);
+    }
+
+    private boolean checkCordinate(final int coordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < FIELD_SIZE;
     }
 }
